f4c292163746bc28e2cfb09d27f4f57a8c61db19
Tejas Ahuja <ahujatejas06@gmail.com>
2019-03-14 00:38:35 +0530

    a lot of changes

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -30,3 +30,46 @@ $(document).ready(function(){
   });
     
 });
+ $(".navlinks").on({
+                    mouseenter: function () {
+                      $('.active').addClass('unactive');
+                      $('.active').removeClass('active');
+                    },
+                    mouseleave: function () {
+                      $('.unactive').addClass('active');
+                      $('.unactive').removeClass('unactive');
+                    }
+                });
+
+            $('#button').click(function() {
+
+            });
+              $(window).scroll(function() {
+                let fade = 1-$(document).scrollTop()*2/$('.main').height();
+                let height = $('.main').height() - $('.nav').height() - 35;
+                let height_color1 = $('.main').height() + ($('.thingsdo').height()/2);
+                let remove_color1 = $('.main').height() + $('.thingsdo').height() + 700;
+
+
+                if ($(document).scrollTop() > height) {
+                    $('.nav').addClass('affix');
+                } else {
+                    $('.nav').removeClass('affix');
+                  }
+
+
+                if($(document).scrollTop() > height_color1 && $(document).scrollTop() < remove_color1) {
+                  $('.thingsdo').addClass('colorwhite');
+                  $('body').addClass('colorblue');
+
+
+                } else {
+                  $('.thingsdo').removeClass('colorwhite');
+                  $('body').removeClass('colorblue');
+
+                }
+
+                $('.main-header').css('opacity', fade);
+                $('.main-social-ul').css('opacity', fade);
+                
+                        });
