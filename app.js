5510015137e2193d8447d3eb4ee58cc059f74802
Tejas Ahuja <ahujatejas06@gmail.com>
2019-03-26 10:44:43 +0530

    removed routing folder

--- app.js
+++ app.js
@@ -4,15 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var minify = require('express-minify');
-var indexRouter = require('./routes/index');
-var academicsRouter = require('./routes/academics');
-
 var app = express();
+
+
 // view engine setup
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'pug');
+
 //middleware to compress CSS
 app.use(minify({
 	cache: false,
@@ -26,14 +24,29 @@ app.use(minify({
 	stylusMatch: false,
 	coffeeScriptMatch: false,
 }));
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', indexRouter);
-app.use('/academics', academicsRouter);
 
+
+
+
+// routing start
+app.get("/", function(req, res, next) {
+	res.render("index", {
+		title: "Home"
+	})
+});
+app.get("/academics", function(req, res, next) {
+	res.render("academics", {
+		title: "academics"
+	})
+});
+// routing end
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
