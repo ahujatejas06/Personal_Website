3a71b7f2818240c03c5784ef92845f0e259e9ea3
Tejas Ahuja <ahujatejas06@gmail.com>
2019-08-11 15:26:51 +0530

    edited projects page

--- public/javascripts/academics.js
+++ public/javascripts/academics.js
@@ -21,6 +21,7 @@ $('.navTrigger').click(function () {
 });
 $(document).ready(function(){
   setPercent();
+  // setWidth();
   });
 $(window).scroll(function() {
             let height = $('.main').height() - $('.nav').height() - 35;
@@ -46,3 +47,37 @@ function setPercent () {
       $(this).css('background',color);
   });
 }
+// function setWidth () {
+//   let number = $('div.layout').data('number');
+//   let width = $(document).width();
+//   let row;
+//   if (width >= 900 && width <=1499)
+//   {
+//     row = 2;
+//   }
+//   else if (width >= 1500 && width <=1999)
+//   {
+//     row = 3;
+//   }
+//   else if (width >= 2000 && width <=2299)
+//   {
+//     row = 4;
+//   }
+//   else if (width >= 2300)
+//   {
+//     row = 5;
+//   }
+//   else {
+//     row = 1;
+//   }
+//   if (number % row != 0)
+//   {
+//     ratio = number%row;
+//     for (let i = 0; i < ratio; ++i)
+//     {
+//       $('div.block::nth-child(' +row-i+'i').css("height", "10px");
+
+
+//     }
+//   }
+// }
